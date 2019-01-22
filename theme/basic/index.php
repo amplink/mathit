9cf3a73145858ca68e1d60c5ca129b0ecd158b7e
amplink <amplink@naver.com>
2019-01-22 10:51:56 +0900

    헤드 수정

--- theme/basic/index.php
+++ theme/basic/index.php
@@ -7,7 +7,8 @@ if (G5_IS_MOBILE) {
     return;
 }
 
-include_once(G5_THEME_PATH.'/head.php');
+//include_once(G5_THEME_PATH.'/head.php');
+include_once('head.php');
 ?>
 
 <h2 class="sound_only">최신글</h2>
