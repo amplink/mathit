98e695cd03ba08e91d2a7924cdc71554a2f732c6
amplink <amplink@naver.com>
2019-01-22 11:14:03 +0900

    헤드 수정

--- admin/index.php
+++ admin/index.php
@@ -67,36 +67,36 @@ include_once(G5_THEME_PATH.'/tail.php');
 
 
 
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>MathIT Admin</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="css/common.css" />
-    <link rel="stylesheet" type="text/css" media="screen" href="css/home.css" />
-    <script src="js/jquery-3.3.1.min.js"></script>
-</head>
-<body>
-    <div class="header">
-        <div class="logo_wrap">
-            <div class="logo"><img src="img/logo.png" alt="logo"></div>
-            <p>ADMIN</p>
-        </div>
-        <nav>
-            <div class="nav_menu"><a href="index.php" class="on">홈</a></div>
-            <div class="nav_menu"><a href="notice_home.php">공지사항관리</a></div>
-            <div class="nav_menu"><a href="academy_option_staff.php">학원별관리</a></div>
-            <div class="nav_menu"><a href="answer_manegement.php">정답지관리</a></div>
-        </nav>
-        <div class="header_right">
-            <div class="user_img"><img src="img/user.png" alt="user_img"></div>
-            <p class="user_id">admin</p>
-            <div class="logout_btn"><a href="login.php">로그아웃</a></div>
-            <div class="pass_change_btn"><a href="home_pass_change.php">비밀번호변경</a></div>
-        </div>
-    </div>
+<!--<!DOCTYPE html>-->
+<!--<html>-->
+<!--<head>-->
+<!--    <meta charset="utf-8" />-->
+<!--    <meta http-equiv="X-UA-Compatible" content="IE=edge">-->
+<!--    <title>MathIT Admin</title>-->
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1">-->
+<!--    <link rel="stylesheet" type="text/css" media="screen" href="css/common.css" />-->
+<!--    <link rel="stylesheet" type="text/css" media="screen" href="css/home.css" />-->
+<!--    <script src="js/jquery-3.3.1.min.js"></script>-->
+<!--</head>-->
+<!--<body>-->
+<!--    <div class="header">-->
+<!--        <div class="logo_wrap">-->
+<!--            <div class="logo"><img src="img/logo.png" alt="logo"></div>-->
+<!--            <p>ADMIN</p>-->
+<!--        </div>-->
+<!--        <nav>-->
+<!--            <div class="nav_menu"><a href="index.php" class="on">홈</a></div>-->
+<!--            <div class="nav_menu"><a href="notice_home.php">공지사항관리</a></div>-->
+<!--            <div class="nav_menu"><a href="academy_option_staff.php">학원별관리</a></div>-->
+<!--            <div class="nav_menu"><a href="answer_manegement.php">정답지관리</a></div>-->
+<!--        </nav>-->
+<!--        <div class="header_right">-->
+<!--            <div class="user_img"><img src="img/user.png" alt="user_img"></div>-->
+<!--            <p class="user_id">admin</p>-->
+<!--            <div class="logout_btn"><a href="login.php">로그아웃</a></div>-->
+<!--            <div class="pass_change_btn"><a href="home_pass_change.php">비밀번호변경</a></div>-->
+<!--        </div>-->
+<!--    </div>-->
     <div class="section">
         <div class="title_box">
             <div class="title_logo_wrap"><img src="img/logo.png" alt="title_logo"></div>
