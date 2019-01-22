d98630f5022d7c8372990d762c2be4daa4ac7cea
amplink <amplink@naver.com>
2019-01-22 10:35:33 +0900

    그누보드 헤드테일 파일

--- admin/home.php
+++ admin/home.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -10,7 +9,6 @@
     <link rel="stylesheet" type="text/css" media="screen" href="css/home.css" />
     <script src="js/jquery-3.3.1.min.js"></script>
 </head>
-
 <body>
     <div class="header">
         <div class="logo_wrap">
