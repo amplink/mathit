4645b91d026feb1d3ae97934cb91e0f02d776bb5
amplink <amplink@naver.com>
2019-01-22 10:46:29 +0900

    헤드 수정

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,6 @@
 <?php
-define('_INDEX_', true);
-if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+////define('_INDEX_', true);
+//if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
 if (G5_IS_MOBILE) {
     include_once(G5_THEME_MOBILE_PATH.'/index.php');
