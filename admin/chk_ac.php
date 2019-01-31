18b14e30c24aae5918e6d92ced28deb40ac25d2a
amplink <amplink@naver.com>
2019-01-31 23:37:28 +0900

    Merge branch 'master' of https://github.com/amplink/mathit
    # Conflicts:
    #	data/cache/latest-free-basic-6-24-serial.php

--- admin/chk_ac.php
+++ admin/chk_ac.php
@@ -12,8 +12,9 @@
     }else {
         $sql = "INSERT INTO `academy` (`client_id`, `event_time`, `admin_id`, `client_name`, `manager_id`, `manager_name`) VALUES ('".$res['client_no']."', CURRENT_TIMESTAMP, '".$res['client_name']."', '".$res['client_name']."', '', '');";
         mysqli_query($connect_db, $sql);
+        echo "<script>alert('".$res['client_name']."학원 등록되었습니다.');</script>";
     }
 
-    echo "<script>alert('".$res['client_name']."학원 등록되었습니다.');</script>";
+
     echo("<script>location.href='./academy_option_add.php';</script>");
 ?>
