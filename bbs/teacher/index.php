c3c432005b44df7494c8bf33f5c6aaa431a175c7
kim-yongseok <gram@amplink.co.kr>
2019-02-11 18:49:24 +0900

    .

--- bbs/teacher/index.php
+++ bbs/teacher/index.php
@@ -1,9 +1,18 @@
+<?php
+include_once('_common.php');
+
+define('_INDEX_', true);
+if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
+
+//if (G5_IS_MOBILE) {
+//    include_once(G5_THEME_MOBILE_PATH.'/index.php');
+//    return;
+//}
+
+?>
 <!DOCTYPE html>
 <html>
-<?
-include_once ('./_common.php');
-exit;
-?>
+
 <head>
 </head>
 
