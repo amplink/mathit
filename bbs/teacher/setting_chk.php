d9af66f4a5ab7c8a740407306e6f45fe22fff2b1
punk9 <49145610+punk9@users.noreply.github.com>
2019-04-02 19:48:29 +0900

    일괄저장 오류 수정

--- bbs/teacher/setting_chk.php
+++ bbs/teacher/setting_chk.php
@@ -5,103 +5,50 @@ $ac = $_SESSION['client_no'];
 $link = "/api/math/teacher_list?client_no=".$ac;
 $r = api_calls_get($link);
 
+$tid = $_POST['tid'];
 $type = $_POST['type'];
-$hm_create = $_POST['hm_create'];
-$hm_mg = $_POST['hm_mg'];
-$score_mg = $_POST['score_mg'];
-$consult_mg = $_POST['consult_mg'];
-$grade_card = $_POST['grade_card'];
-$notice = $_POST['notice'];
-$admin_menu = $_POST['admin_menu'];
+
+$hm_create = explode(",",implode(",", $_POST['hm_create']));
+$hm_mg = explode(",",implode(",", $_POST['hm_mg']));
+$score_mg = explode(",",implode(",", $_POST['score_mg']));
+$consult_mg = explode(",",implode(",", $_POST['consult_mg']));
+$grade_card = explode(",",implode(",", $_POST['grade_card']));
+$notice = explode(",",implode(",", $_POST['notice']));
+$admin_menu = explode(",",implode(",", $_POST['admin_menu']));
 
 $sql = "select * from `academy`;";
 $result = sql_query($sql);
 $manager = array();
 $i=0;
 
-while($res = mysqli_fetch_array($result)) {
-    $manager[$i] = $res['manager_id'];
-    $i++;
-}
 
-$sql = "select * from `teacher_setting`;";
-$result = sql_query($sql);
-$user_list = array();
+for($i=0;$i<count($tid);$i++){ 
 
-$i=0;
-while($res = mysqli_fetch_array($result)) {
-    $user_list[$i] = $res['t_id'];
-    $i++;
-}
+   $hc = (in_array($tid[$i], $hm_create))?"1":"0";
+   $hm = (in_array($tid[$i], $hm_mg))?"1":"0";
+   $sm = (in_array($tid[$i], $score_mg))?"1":"0";
+   $cm = (in_array($tid[$i], $consult_mg))?"1":"0";
+   $gc = (in_array($tid[$i], $grade_card))?"1":"0";
+   $nt = (in_array($tid[$i], $notice))?"1":"0";
+   $am = (in_array($tid[$i], $admin_menu))?"1":"0";
 
-for($i=1; $i<count($r); $i++) {
-    $c_hm_create = 0;
-    $c_hm_mg = 0;
-    $c_score_mg = 0;
-    $c_consult_mg = 0;
-    $c_grade_card = 0;
-    $c_notice = 0;
-    $c_admin_menu = 0;
+	sql_query(
+		"update teacher_setting set 
 
-    $sql = "select * from `teacher_setting` where `t_id` = '".$r[$i][0]."';";
-    $result = mysqli_query($connect_db, $sql);
-    if($result) {
-        $sql = "delete from `teacher_setting` where `t_id` = '".$r[$i][0]."'";
-        mysqli_query($connect_db, $sql);
-
-        if($type[$i-1] == "전임강사") {
-            $c_hm_create = 1;
-            $c_hm_mg = 1;
-            $c_score_mg = 1;
-            $c_consult_mg = 1;
-            $c_grade_card = 1;
-            $c_notice = 1;
-
-            for($j=0; $j<count($r); $j++) {
-                if($hm_create[$j] == $r[$i][0]) $c_hm_create = 1;
-                if($hm_mg[$j] == $r[$i][0]) $c_hm_mg = 1;
-                if($score_mg[$j] == $r[$i][0]) $c_score_mg = 1;
-                if($consult_mg[$j] == $r[$i][0]) $c_consult_mg = 1;
-                if($grade_card[$j] == $r[$i][0]) $c_grade_card = 1;
-                if($notice[$j] == $r[$i][0]) $c_notice = 1;
-                if($admin_menu[$j] == $r[$i][0]) $c_admin_menu = 1;
-            }
-        } else{
-            $c_hm_mg = 1;
-            $c_grade_card = 1;
-            $c_consult_mg = 1;
-            $c_score_mg = 1;
-
-            for($j=0; $j<count($r); $j++) {
-                if($hm_create[$j] == $r[$i][0]) $c_hm_create = 1;
-                if($hm_mg[$j] == $r[$i][0]) $c_hm_mg = 1;
-                if($score_mg[$j] == $r[$i][0]) $c_score_mg = 1;
-                if($consult_mg[$j] == $r[$i][0]) $c_consult_mg = 1;
-                if($grade_card[$j] == $r[$i][0]) $c_grade_card = 1;
-                if($notice[$j] == $r[$i][0]) $c_notice = 1;
-                if($admin_menu[$j] == $r[$i][0]) $c_admin_menu = 1;
-            }
-        }
-
-        for($j=0; $j<count($manager); $j++) {
-            if($manager[$j] == $r[$i][1]) {
-                $c_hm_create = 1;
-                $c_hm_mg = 1;
-                $c_score_mg = 1;
-                $c_consult_mg = 1;
-                $c_grade_card = 1;
-                $c_notice = 1;
-                $c_admin_menu = 1;
-            }
-        }
-
-        $sql = "INSERT INTO `teacher_setting` (`seq`, `t_id`, `t_name`, `type`, `hm_create`, `hm_mg`, `score_mg`, `consult_mg`, `grade_card`, `notice`, `admin_menu`, `event_time`) 
-VALUES (NULL, '".$r[$i][0]."', '".$r[$i][3]."', '".$type[$i-1]."', '$c_hm_create', '$c_hm_mg', '$c_score_mg', '$c_consult_mg', '$c_grade_card', '$c_notice', '$c_admin_menu', CURRENT_TIMESTAMP);";
-        mysqli_query($connect_db, $sql);
-    }
+			hm_create = $hc,
+			hm_mg = $hm,
+			score_mg = $sm,
+			consult_mg = $cm,
+			grade_card = $gc,
+			notice = $nt,
+			admin_menu = $am
+	     where 
+            t_id = '$tid[$i]'"
+	);
 
 }
+
 alert_msg("등록이 완료되었습니다.");
-location_href("./setting.php");
+location_href("./setting2.php");
 
 ?>
