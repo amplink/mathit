07267d85ae946803857731ea6b90ebb90fa2ec33
punk9 <crocus999@nate.com>
2019-04-25 21:53:07 +0900

    원청피드백 수정

--- bbs/teacher/record_management_add_chk.php
+++ bbs/teacher/record_management_add_chk.php
@@ -23,13 +23,15 @@ $d_id = $_POST['d_id'];
 $c_id = $_POST['c_id'];
 $s_id = $_POST['s_id'];
 
-$sql = "delete * from `teacher_score` where `title` = '$title' and `test_genre` = '$test_genre' and `class` = '$class';";
-sql_query($sql);
+//$sql = "delete * from `teacher_score` where `title` = '$title' and `test_genre` = '$test_genre' and `class` = '$class';";
+//sql_query($sql);
 
+$j = 0;
 for($i=0; $i<count($student); $i++) {
     $sql = "INSERT INTO `teacher_score` (`client_id`, `d_uid`, `c_uid`, `s_uid`, `class`, `grade`, `year`, `quarter`, `d_order`, `test_genre`, `title`, `date`, `standard`, `sub_score1`, `sub_score2`, `sub_score3`, `score1`, `score2`, `score3`, `teacher`, `student_id`, `student`, `event_time`) 
 VALUES ('$_SESSION[client_no]', '$d_id', '$c_id', '$s_id', '$class', '$grade', '$year', '$quarter', '$d_yoie', '$test_genre', '$title', '$date', '$standard', '$sub_score1', '$sub_score2', '$sub_score3', '$score_add1[$i]', '$score_add2[$i]', '$score_add3[$i]', '$teacher', '$student_id[$i]', '$student[$i]', CURRENT_TIMESTAMP);";
-    sql_query($sql);
+    $result = sql_query($sql);
+    if($result == 1) $j++;
 }
 
 $ac = $_SESSION['client_no'];
@@ -44,6 +46,10 @@ for($i=1; $i<count($r); $i++) {
 $sql = "insert into `alarm` set `seq`='', `content`='새로운 성적표가 등록되었습니다.', `table_name`='score', `target`='관리자', `chk`='0', `datetime`=CURRENT_TIMESTAMP";
 sql_query($sql);
 
-alert_msg("등록이 완료되었습니다.");
+if(count($student) != $j){
+    alert_msg("이미 등록되어진 성적표가 있습니다.");
+}else{
+    alert_msg("등록이 완료되었습니다.");
+}
 location_href("./record_management_list.php");
 ?>
