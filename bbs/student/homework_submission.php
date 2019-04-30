3f1e2b23f9cc2c8d7d8652ed1386c14e63d18b26
punk9 <crocus999@nate.com>
2019-05-01 03:37:52 +0900

    .

--- bbs/student/homework_submission.php
+++ bbs/student/homework_submission.php
@@ -26,7 +26,7 @@ if($res['current_status'] == '' || $res['current_status'] == 's1'){
     ?>
     <!--section-->
     <section>
-        <div class="bigimg" style="position:absolute;width:100%;height:100vh;z-index:999;display:none"></div>
+        <div class="bigimg" style="position:absolute;width:100%;height:100vh;z-index:999;background-color:red;display:none"></div>
         <div class="head_p">
             <p class="head_title">숙제제출</p>
             <div class="back_btn"><a href="homework_ing.php"><img src="img/back_btn.png" alt="back_btn_icon"></a></div>
@@ -49,7 +49,7 @@ if($res['current_status'] == '' || $res['current_status'] == 's1'){
             <form action='./upload_photo.php' method="post" name="photoForm" id="photoForm" enctype="multipart/form-data">
                 <input type="hidden" name="id" value="<?=$_GET['no']?>">
                 <input type="hidden" name="sort" id="sort" value="">
-                <div class="detail_head"><input multiple="multiple" name="files[]" id="files" type="file" style="display:none"/>
+                <div class="detail_head"><input multiple name="files[]" id="files" type="file" style="display:none"/>
                     <div class="check_btn"  onclick='photosel()'><a><img src="img/check_btn.png" alt="check_btn_icon" style="width: 25px; float: left;"  id="imgReg"><span class="complete">등록</span></a></div>
                     <p><span>    등록 버튼을 눌러 숙제 이미지를 추가한 후</span><span>완료 버튼을 눌러 저장해 주세요</span></p>
                 </div>
@@ -64,7 +64,7 @@ if($res['current_status'] == '' || $res['current_status'] == 's1'){
             </form>
 
             <div class="detail_footer">
-                <p><span>현재 등록된 이미지 개수 : </span><span id="num">1</span><span>장</span></p>
+                <p><span>현재 등록된 이미지 개수 : </span><span id="num">0</span><span>장</span></p>
                 <div class="check_btn" id="check_btn" onClick="save()"><a><img src="img/check_btn.png" alt="check_btn_icon" style="width: 25px; float: left;"><span class="complete">완료</span></a></div>
             </div>
             <div class="decoration">
@@ -86,6 +86,7 @@ if($res['current_status'] == '' || $res['current_status'] == 's1'){
     <script type="text/javascript" src="./js/jquery.form.min.js" ></script>
     <script type="text/javascript" src="./js/jquery-ui.js" ></script>
     <script type="text/javascript" src="./js/jquery.ui.touch-punch.min.js" ></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-touch-events/1.0.5/jquery.mobile-events.js"></script>
 
     <script>
 
@@ -93,58 +94,97 @@ if($res['current_status'] == '' || $res['current_status'] == 's1'){
 
             $('#files').change(function(){
                 fileBuffer = [];
-                const target = document.getElementsByName('files[]');
+                //const target = document.getElementsByName('files[]');
+                var files   = document.querySelector('input[type=file]').files;
 
-                Array.prototype.push.apply(fileBuffer, target[0].files);
-                var html = '';
-                var i = $('.photo_box').length;
-                var j = 1;
-                $.each(target[0].files, function(index, file){
-                    const fileName = file.name;
-                    html += '<div class="file"  id="img_'+j+'">';
+                //Array.prototype.push.apply(fileBuffer, target[0].files);
 
-                    html += '<div class="photo_box">';
-                    html += '<div class="camera_icon" style="width:100%;">';
-                    //html += '<img src="img/camera.png" alt="camera_icon"></div>';
-                    html += '<img src="'+URL.createObjectURL(file)+'" height="70" style="margin-top:-25px"></div>';
-                    html += '</div>';
-                    //html += '<img src="'+URL.createObjectURL(file)+'" width="30" height="30">'
-                    html += '<span style="font-size:13px">'+i+'</span>';
-                    //html += '<span> <input type="text" style="width:250px/"></span>';
-                    html += '<span id="removeImg">X</span>';
-                    html += '</div>';
-                    const fileEx = fileName.slice(fileName.indexOf(".") + 1).toLowerCase();
-                    if(fileEx != "jpg" && fileEx != "png" &&  fileEx != "gif"){
-                        alert("파일은 (jpg, png, gif) 형식만 등록 가능합니다.");
-                        //resetFile();
-                        return false;
+                var i = $('.photo_box').length + 1;
+
+                function readAndPreview(file) {
+
+                    var j = 1;
+                    var html = '';
+
+                    // Make sure `file.name` matches our extensions criteria
+                    if ( /\.(jpe?g|png|gif)$/i.test(file.name) ) {
+                        var reader = new FileReader();
+
+                        reader.addEventListener("load", function () {
+                            var image = new Image();
+                            image.height = 100;
+                            image.title = file.name;
+                            image.src = this.result;
+                            //preview.appendChild( image );
+
+
+                            html += '<div class="file"  id="img_'+i+'">';
+                            html += '<div class="photo_box">';
+                            html += '<div class="camera_icon" style="width:100%;">';
+                            //html += '<img src="img/camera.png" alt="camera_icon"></div>';
+                            html += '<img src="'+image.src+'" height="70" style="margin-top:-25px"></div>';
+                            html += '</div>';
+                            //html += '<img src="'+URL.createObjectURL(file)+'" width="30" height="30">'
+                            html += '<span style="font-size:13px">'+i+'</span>';
+                            //html += '<span> <input type="text" style="width:250px/"></span>';
+                            html += '<span id="removeImg">X</span>';
+                            html += '</div>';
+
+                            ++i;
+
+                            $("#num").text(i-1);
+                            $('.photo_section').append(html);
+                        }, false);
+
+                        reader.readAsDataURL(file);
                     }
 
-                    i++;
-                    j++;
-                });
-                var num = $('.photo_box').length + i;
-                $('.photo_section').append(html);
-                $("#num").text(num);
+                }
+
+                if (files) {
+                    [].forEach.call(files, readAndPreview);
+                }
+
+
+
+                /*
+                                $.each(target[0].files, function(index, file){
+                                    const fileName = file.name;
+                                    html += '<div class="file"  id="img_'+j+'">';
+
+                                    html += '<div class="photo_box">';
+                                    html += '<div class="camera_icon" style="width:100%;">';
+                                    //html += '<img src="img/camera.png" alt="camera_icon"></div>';
+                                    html += '<img src="'+URL.createObjectURL(file)+'" height="70" style="margin-top:-25px"></div>';
+                                    html += '</div>';
+                                    //html += '<img src="'+URL.createObjectURL(file)+'" width="30" height="30">'
+                                    html += '<span style="font-size:13px">'+i+'</span>';
+                                    //html += '<span> <input type="text" style="width:250px/"></span>';
+                                    html += '<span id="removeImg">X</span>';
+                                    html += '</div>';
+                                    const fileEx = fileName.slice(fileName.indexOf(".") + 1).toLowerCase();
+                                    if(fileEx != "jpg" && fileEx != "png" &&  fileEx != "gif"){
+                                        alert("파일은 (jpg, png, gif) 형식만 등록 가능합니다.");
+                                        //resetFile();
+                                        return false;
+                                    }
+
+                                    i++;
+                                    j++;
+                                });*/
+                //var num = i;
+                // $('.photo_section').append(html);
+
             });
 
-            $(document).on('click', '#removeImg', function(){
+            $(document).on('touchend', '#removeImg', function(){
                 const fileIndex = $(this).parent().index();
                 fileBuffer.splice(fileIndex,1);
                 $('.photo_section>div:eq('+fileIndex+')').remove();
             });
 
 
-            $(document).on('click', '.camera_icon', function(){
-
-                var img = $(this).children("img");
-                var img_src = img.attr("src");
-                $("#bigimg").show();
-                $("#bigimg").html("<img src='"+img_src+"'>");
-
-            });
-
-            $(document).on('click', '.camera_icon', function(){
+            $(document).on('touchend', '.camera_icon', function(){
 
                 var img = $(this).children("img");
                 var img_src = img.attr("src");
@@ -154,7 +194,7 @@ if($res['current_status'] == '' || $res['current_status'] == 's1'){
             });
 
 
-            $(document).on('click', '.bigimg', function(){
+            $(document).on('touchend', '.bigimg', function(){
                 $(this).hide();
             });
 
@@ -183,8 +223,10 @@ if($res['current_status'] == '' || $res['current_status'] == 's1'){
                         alert("잠시 후 다시 시도해 주십시오.") ;
                     },
                     success : function(result){
-                        if(result == "success") alert('정상 제출 되었습니다.');
-                        else if(result == "err") alert('잠시 후 다시 시도해 주십시오.');
+                        if(result == "success") alert('사진이 정상 등록 되었습니다.');
+                        else if(result == "err") alert('사진이 정상 등록 되지 않았습니다.');
+
+                        location.href = "homework_submission.php?no=<?=$_GET['no']?>";
                     }
                 }).submit();
 
--- bbs/student/upload_photo.php
+++ bbs/student/upload_photo.php
@@ -1,10 +1,12 @@
 <?php
 include_once ('_common.php');
 
-$i = 1;
+$i = 0;
 
 $sort = str_replace("img[]=","",$_POST['sort']);
 $sort_arr = explode("&", $sort);
+
+$tot = count($_FILES['files']['name']);
 
 foreach ($_FILES['files']['name'] as $f => $name) {
 
@@ -21,7 +23,9 @@ foreach ($_FILES['files']['name'] as $f => $name) {
     $name = $_FILES['files']['name'][$f];
     $uploadName = explode('.', $name);
 
-    // $fileSize = $_FILES['upload']['size'][$f];
+    $fileSize = $_FILES['upload']['size'][$f];
+    if($fileSize > 5242880) break;
+
     // $fileType = $_FILES['upload']['type'][$f];
     $uploadname = time().$f.'.'.$uploadName[1];
     $ext = substr($uploadname, strpos($uploadname, ".")+1);
@@ -39,13 +43,11 @@ foreach ($_FILES['files']['name'] as $f => $name) {
         $sql = "INSERT INTO `upload_photo` (`id`, `file_name`, `org_file_name`, `sort`, reg_date)
 			VALUES ('$id', '$filename', '$name', '$sno', now());";
         $result = sql_query($sql);
-        if($result) echo 'success';
-        else        echo 'err';
-    }else{
-        echo 'err';
+        if($result) $i++;
     }
-    $i++;
 }
 
+if($tot == $i) echo "success";
+else if($tot > $i) echo "err";
 
 ?>
