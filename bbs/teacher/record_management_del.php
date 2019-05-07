b4f227e6e0654d27f0106840177a40707ba4f43a
kim-yongseok <gram@amplink.co.kr>
2019-05-07 15:52:37 +0900

    .

--- bbs/teacher/record_management_del.php
+++ bbs/teacher/record_management_del.php
@@ -21,6 +21,17 @@ if(count($chk_list) > 0) {
     for($i=0; $i<count($student); $i++) {
         $sql = "update `teacher_score` set `score1` = '$score1[$i]', `score2`='$score2[$i]', `score3`='$score3[$i]' where `student_id`='$student[$i]' and test_genre = '$test_genre' and d_uid= '$d_uid' and c_uid= '$c_uid' and s_uid= '$s_uid'";
         sql_query($sql);
+        $sql = "select * from `fcm` where `uid`='".$student[$i]."';";
+        $result = sql_query($sql);
+        $tokens = array();
+        while($res = mysqli_fetch_array($result)) {
+            $sql1 = "select `push_alarm` from `student_table` where `id`='".$res['uid']."';";
+            $result1 = sql_query($sql1);
+            $res1 = mysqli_fetch_array($result1);
+            if($res1['push_alarm']) $tokens[] = $res['token'];
+        }
+        $message = "성적표가 수정되었습니다.";
+        send_notification($tokens, $message);
     }
     echo "1";
 }
